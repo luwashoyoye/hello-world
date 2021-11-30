b67b0a13445368c939c097942ffe5eb3d63a7a49
Ifeoluwa Shoyoye <ifeoluwa.shoyoye@kysu.edu>
2021-11-30 10:58:02 -0500

    Personalize program

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello World." << std::endl;
+    std::cout << "What is your name? ";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello " << name << std::endl;
     return 0;
 }
